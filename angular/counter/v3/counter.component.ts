0e08198632f8e35b66f81c4275d62ecfaed34d2f
goodsungjin <mirepach@naver.com>
2019-06-17 20:29:41 +0900

    add tabsUi

--- angular/counter/v3/counter.component.ts
+++ angular/counter/v3/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -19,7 +19,24 @@ import { Component } from '@angular/core';
       font-size: 24px;
       color: #3f51b5;
     }
-  `]
+
+    button {
+      padding: 5px 10px;
+      font-size: 24px;
+      border-radius: 5px;
+      color: #3f51b5;
+      border-color: #3f51b5;
+      outline: none;
+      cursor: pointer;
+    }
+  
+    .counter {
+      width: 50px;
+      text-align: center;
+    }
+  `],
+
+  encapsulation: ViewEncapsulation.None
 })
 
 export class CounterComponent {
